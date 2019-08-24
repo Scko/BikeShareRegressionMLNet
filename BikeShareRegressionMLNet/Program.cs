379a2b2ab02b87f790f59206de28109f8643264e
Carl <carl.chester.lloyd@gmail.com>
2019-08-23 22:51:41 -0400

    Split and CrossValidation

--- BikeShareRegressionMLNet/Program.cs
+++ BikeShareRegressionMLNet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.ML;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace LinearRegressionMLNet
 {
@@ -8,33 +9,62 @@ namespace LinearRegressionMLNet
     {
         static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "bkTrain.csv");
         static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "bkTest.csv");
-
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "day.csv");
         static void Main(string[] args)
         {
-            MLContext mlContext = new MLContext(seed: 0);
+            var mlContext = new MLContext(seed: 0);
+            var estimator = mlContext.Regression.Trainers.FastTree();
 
-            var model = Train<BikeShare>(mlContext, _trainDataPath);
+            var model = Train<BikeShare>(mlContext, _dataPath, estimator);
 
-            Evaluate<BikeShare>(mlContext, model);
+            CrossValidation<BikeShare>(mlContext, model, _dataPath, estimator);
+            //Evaluate<BikeShare>(mlContext, model);
 
-            TestPrediction(mlContext, model);
+            //TestPrediction(mlContext, model);
         }
 
-        public static ITransformer Train<T>(MLContext mlContext, string dataPath)
+        public static ITransformer Train<T>(MLContext mlContext, string dataPath, IEstimator<ITransformer> estimator)
         {
             IDataView dataView = mlContext.Data.LoadFromTextFile<T>(dataPath, hasHeader: true, separatorChar: ',');
+            DataOperationsCatalog.TrainTestData dataSplit = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            IDataView trainData = dataSplit.TrainSet;
+            IDataView testData = dataSplit.TestSet;
 
             var pipeline = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "cnt")
                     .Append(mlContext.Transforms.Concatenate("Features", "season", "yr", "mnth", "holiday", "weekday", "workingday", "weathersit", "atemp", "temp", "hum", "windspeed"))
                     //.Append(mlContext.Transforms.Concatenate("Features", "season", "yr", "mnth", "holiday", "workingday", "weathersit", "temp", "hum", "windspeed"))
-                    .Append(mlContext.Regression.Trainers.FastTree());
+                    .Append(estimator);
 
             Console.WriteLine("=============== Create and Train the Model ===============");
-            var model = pipeline.Fit(dataView);
+            var model = pipeline.Fit(trainData);
             Console.WriteLine("=============== End of training ===============");
             Console.WriteLine();
 
             return model;
+        }
+
+        private static ITransformer CrossValidation<T>(MLContext mlContext, ITransformer model, string dataPath, IEstimator<ITransformer> estimator)
+        {
+            //IEstimator<ITransformer> estimator = mlContext.Regression.Trainers.FastTree();
+            IDataView dataView = mlContext.Data.LoadFromTextFile<T>(dataPath, hasHeader: true, separatorChar: ',');
+            IDataView transformedData = model.Transform(dataView);
+
+            var cvResults = mlContext.Regression.CrossValidate(transformedData, estimator, numberOfFolds: 5);
+            var rSquared = cvResults.Sum(fold => fold.Metrics.RSquared) / cvResults.Count;
+            var rmse = cvResults.Sum(fold => fold.Metrics.RootMeanSquaredError) / cvResults.Count;
+
+            ITransformer[] models = cvResults.OrderByDescending(fold => fold.Metrics.RSquared).Select(fold => fold.Model).ToArray();
+            ITransformer topModel = models[0];
+
+            Console.WriteLine();
+            Console.WriteLine($"*************************************************");
+            Console.WriteLine($"*       Model quality metrics cross validation        ");
+            Console.WriteLine($"*------------------------------------------------");
+            Console.WriteLine($"*       RSquared Score: {rSquared:0.##}     ");
+            Console.WriteLine($"*       Root Mean Squared Error:      {rmse:#.##}");
+            Console.WriteLine($"*************************************************");
+
+            return topModel;
         }
 
         private static void Evaluate<T>(MLContext mlContext, ITransformer model)
